08daa08c3ca0fb3d10339a810cf9a0fb5ec35c8c
ozkanonur <work@onurozkan.dev>
2023-02-02 23:49:58 +0300

    decrease the font size by 1
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font:size=12:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font:size=11:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
