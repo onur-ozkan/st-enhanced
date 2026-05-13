25fd67c02859822a966d3dc1adbd218647329946
Onur Özkan <work@onurozkan.dev>
2026-05-13 15:53:44 +0300

    drop vim modes
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- config.h
+++ config.h
@@ -219,9 +219,7 @@ static MouseShortcut mshortcuts[] = {
 	/* mask button function argument release */
 	{XK_ANY_MOD, Button2, selpaste, {.i = 0}, 1},
 	{ShiftMask, Button4, ttysend, {.s = "\033[5;2~"}},
-	{XK_ANY_MOD, Button4, ttysend, {.s = "\031"}},
 	{ShiftMask, Button5, ttysend, {.s = "\033[6;2~"}},
-	{XK_ANY_MOD, Button5, ttysend, {.s = "\005"}},
 };
 
 /* Internal keyboard shortcuts. */
